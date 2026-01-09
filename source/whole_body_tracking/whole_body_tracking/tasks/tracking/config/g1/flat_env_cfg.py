4de75a4278eaca358d500205c0142cc0848dd0b3
aaaxlh <2940562534@qq.com>
2026-01-09 21:45:09 +0800

    修改flat_env_cfg.py以适配23dofs模型link跟踪

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/flat_env_cfg.py
@@ -15,19 +15,15 @@ class G1FlatEnvCfg(TrackingEnvCfg):
         self.commands.motion.anchor_body_name = "torso_link"
         self.commands.motion.body_names = [
             "pelvis",
-            "left_hip_roll_link",
-            "left_knee_link",
-            "left_ankle_roll_link",
-            "right_hip_roll_link",
-            "right_knee_link",
-            "right_ankle_roll_link",
+            "left_hip_roll_link","left_knee_link","left_ankle_roll_link",
+            "right_hip_roll_link","right_knee_link","right_ankle_roll_link",
             "torso_link",
-            "left_shoulder_roll_link",
-            "left_elbow_link",
-            "left_wrist_yaw_link",
-            "right_shoulder_roll_link",
-            "right_elbow_link",
-            "right_wrist_yaw_link",
+            "left_shoulder_roll_link","left_elbow_link",
+            # "left_wrist_yaw_link",
+            "left_wrist_roll_rubber_hand",
+            "right_shoulder_roll_link","right_elbow_link",
+            # "right_wrist_yaw_link",
+            "right_wrist_roll_rubber_hand", 
         ]
 
 
